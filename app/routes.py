43422ae7f96cc721d6d587590b9694fd9c4ebafe
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-12-08 22:18:32 -0600

    updated routes for new reserve_seat.html

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,6 @@ bp = Blueprint("main", __name__)
 @bp.route("/")
 def main_menu():
     return render_template("main_menu.html")
+@bp.route("/reserve")
+def reserve_seat():
+    return render_template("reserve_seat.html")
