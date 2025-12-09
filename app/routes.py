03a5c3a94cd963fb0871c0e89a4410ed819d1bc7
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-12-08 22:14:15 -0600

    added css to make it look pretty

--- app/routes.py
+++ app/routes.py
@@ -2,4 +2,4 @@ from flask import Blueprint, render_template
 bp = Blueprint("main", __name__)
 @bp.route("/")
 def main_menu():
-    return "<h1>Bus Reservation - Main Menu</h1><p>Next: implement templates and routes.</p>"
+    return render_template("main_menu.html")
