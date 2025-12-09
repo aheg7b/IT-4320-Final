df94c057dc3e2ce7dc7e716853c1fa4e34bc7494
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-12-08 22:49:42 -0600

    added in admin login and admin page

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,16 @@ def main_menu():
 @bp.route("/reserve")
 def reserve_seat():
     return render_template("reserve_seat.html")
+@bp.route("/admin/login", methods=["GET", "POST"])
+def admin_login():
+    if "POST" == str(request.method):
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # For now, allow only admin/admin
+        #THIS IS WHERE WE WILL ADD LOGIC FOR LOGIN
+        if username == "admin" and password == "admin":
+            return render_template("admin_dashboard.html")
+        else:
+            error = "Invalid credentials"
+            return render_template("admin_login.html", error=error)
+    return render_template("admin_login.html")
